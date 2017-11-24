8a3d4fe6cfd914b482f43277af379016b6f28745
Erik <erikwjonsson@gmail.com>
2017-11-24 23:00:38 +0100

    Set ApplicationController to inherit from ::Base. Added json-api resources gem. Added protect_against_forgery. Set eager_loading to true.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
+  # By allowing inheriting from ::Base more is modules are included.
+  protect_from_forgery with: :null_session
 end
