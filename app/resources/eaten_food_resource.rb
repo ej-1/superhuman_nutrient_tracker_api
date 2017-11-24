edb2f72ae00cd4631b06c2e9e6b7d2cfb07aa7b8
Erik <erikwjonsson@gmail.com>
2017-11-25 00:07:50 +0100

    Added more attributes to User and EatenFood resources.

--- app/resources/eaten_food_resource.rb
+++ app/resources/eaten_food_resource.rb
@@ -1,5 +1,8 @@
 class EatenFoodResource < JSONAPI::Resource
   attributes :amount
+  attributes :metric_used
+  attributes :eaten_at
+
   belongs_to :user
   filter :user # whats this?
 end
--- app/resources/user_resource.rb
+++ app/resources/user_resource.rb
@@ -1,4 +1,8 @@
 class UserResource < JSONAPI::Resource
   attributes :first_name
+  attributes :last_name
+  attributes :email
+  attributes :api_key
+
   has_many :eaten_foods
 end
